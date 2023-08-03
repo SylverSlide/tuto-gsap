14224faeb69cd914109ea162926cafd61e0e7d31
n.zitouni <n.zitouni@capency.com>
2023-08-03 17:27:07 +0200

    implementation gsap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
-import { TimelineLite, Back, Power1 } from 'gsap';
+import { Component, OnInit } from '@angular/core';
+import { TimelineLite, Back, Power1, gsap } from 'gsap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    let timeline = gsap.timeline({ repeat: -1, yoyo: true });
+    timeline.to('#circle', { y: 250 });
+    timeline.to('#circle', { x: 250 });
+    timeline.to('#circle', { y: 0 });
+    timeline.to('#circle', { x: -250 });
+  }
+}
