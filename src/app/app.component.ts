07175ef0044cfed3898cf893e9f1f15850c63467
n.zitouni <n.zitouni@capency.com>
2023-08-03 17:01:05 +0200

    import gsap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TimelineLite, Back, Power1 } from 'gsap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
   title = 'gsap-tuto';
